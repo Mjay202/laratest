7231cb98dd83564ea861f31ec21e9f34de4ebe22
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-03-01 22:31:09 +0100

    added value old

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -20,6 +20,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value="{{old('company')}}"
                             />
 
                             @error('company')
@@ -37,6 +38,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{old('title')}}"
                             />
                             
                             @error('title')
@@ -55,6 +57,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
+                                value="{{old('location')}}"
                             />
                             
                             @error('location')
@@ -70,6 +73,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value="{{old('email')}}"
                             />
                             
                             @error('email')
@@ -88,6 +92,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value="{{old('website')}}"
                             />
                             
                             @error('website')
@@ -104,6 +109,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                                value="{{old('tags')}}"
                             />
                             
                             @error('tags')
@@ -119,6 +125,7 @@
                                 type="file"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="logo"
+                                value="{{old('logo')}}"
                             />
                             
                             @error('logo')
@@ -138,6 +145,7 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
+                                value="{{old('description')}}"
                             ></textarea>
                             
                             @error('description')
